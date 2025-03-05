7c4b84ed05cf1c3e3bbf2ceec5ae062fd4bbe49a
mo2561057 <moritz.mendel12@gmail.com>
2025-03-05 16:41:11 +0200

    Fix algo options in the recursive case

--- src/gaussian_rep/entry_point.py
+++ src/gaussian_rep/entry_point.py
@@ -35,6 +35,6 @@ def recursively_estimate_multivariate_dual(
 
     if len(S) > 1:
         return recursively_estimate_multivariate_dual(
-            S[1:],s[1:], T, y[:,1:], np.concatenate([y_current.reshape(y_current.shape[0],1),X],axis=1), beta, algorithm=algorithm)    
+            S[1:],s[1:], T, y[:,1:], np.concatenate([y_current.reshape(y_current.shape[0],1),X],axis=1), beta, algorithm=algorithm, algorithm_options=algorithm_options)    
     else:
         return beta
